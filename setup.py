2013958579d44584269ffe511c2a8781b66a240e
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-10-26 15:03:09 +0200

    Add dependency on muninn

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='muninn-sentinel5p',
     version='2.0',
-    py_modules=['muninn_sentinel5p']
+    py_modules=['muninn_sentinel5p'],
+    install_requires=['muninn'],
 )
